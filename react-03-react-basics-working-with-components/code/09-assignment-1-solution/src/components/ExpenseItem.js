93fe3884d474776007768e8ced4d367b9180bb73
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-03-27 00:38:40 +0000

    Local is confused vs remote - C+Ping again to align

--- react-03-react-basics-working-with-components/code/09-assignment-1-solution/src/components/ExpenseItem.js
+++ react-03-react-basics-working-with-components/code/09-assignment-1-solution/src/components/ExpenseItem.js
@@ -1,16 +1,21 @@
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const { date, title, amount } = props;
+
   return (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+    <>
+      <div className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">£{amount}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default ExpenseItem;
