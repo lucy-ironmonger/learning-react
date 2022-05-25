003ecce5777bb649c1450e42c9792b5ae6ef9d6b
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-05-25 22:40:38 +0100

    Experimenting with useState

--- lucy-code/expense-tracker/src/components/Expenses/ExpenseItem.js
+++ lucy-code/expense-tracker/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
+import {useState} from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+
+  const [title, setTitle] = useState(props.title)
+  console.log('ExpenseItem evaluated by React');
+
+  const clickHandler = () => {
+   setTitle("Updated!")
+   console.log("React is updating the component")
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
