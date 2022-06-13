07988a78a11638e11bb60f298609f40043818e0f
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-06-13 21:23:20 +0100

    Assignment 2 submission

--- lucy-code/expense-tracker/src/components/Expenses/Expenses.js
+++ lucy-code/expense-tracker/src/components/Expenses/Expenses.js
@@ -1,18 +1,42 @@
-import './Expenses.css'
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from 'react';
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
-  const { items } = props
+  const { items } = props;
 
-  return ( 
+  const [selectedYear, setSelectedYear] = useState('');
+  const dateHandler = (year) => {
+    setSelectedYear(year);
+  };
+
+  return (
     <Card className="expenses">
-      <ExpenseItem title={items[0].title} amount={items[0].amount} date={items[0].date} />
-      <ExpenseItem title={items[1].title} amount={items[1].amount} date={items[1].date} />
-      <ExpenseItem title={items[2].title} amount={items[2].amount} date={items[2].date} />
-      <ExpenseItem title={items[3].title} amount={items[3].amount} date={items[3].date} />
+      <ExpensesFilter selectedYear={selectedYear} onDateSelect={dateHandler} />
+      <ExpenseItem
+        title={items[0].title}
+        amount={items[0].amount}
+        date={items[0].date}
+      />
+      <ExpenseItem
+        title={items[1].title}
+        amount={items[1].amount}
+        date={items[1].date}
+      />
+      <ExpenseItem
+        title={items[2].title}
+        amount={items[2].amount}
+        date={items[2].date}
+      />
+      <ExpenseItem
+        title={items[3].title}
+        amount={items[3].amount}
+        date={items[3].date}
+      />
     </Card>
-   );
+  );
 }
- 
+
 export default Expenses;
